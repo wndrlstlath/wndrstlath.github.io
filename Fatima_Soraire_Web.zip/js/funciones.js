c66c2e2cc9519b807fc7f56595696d9d68561bed
wndrlstlath <126731046+wndrlstlath@users.noreply.github.com>
2023-03-01 22:05:55 -0300

    initial commit

---
+++ Fatima_Soraire_Web.zip/js/funciones.js
@@ -0,0 +1,14 @@
+document.getElementById("boton numero").addEventListener('click', function () {
+    document.getElementById("demo").innerHTML = "Número de telefono: (819) 533-3436";
+    
+})
+document.getElementById("boton email").addEventListener('click', function () {
+    document.getElementById("demo1").innerHTML = "E-mail: addison.brooks@example.com";
+    
+})
+document.getElementById("boton direccion").addEventListener('click', function () {
+    document.getElementById("demo2").innerHTML = "Dirección: 8593 Nowlin Rd";
+    
+})
+
+document.body.style.backgroundColor = 'cornflowerblue';
